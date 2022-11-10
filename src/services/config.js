40da102efaa77af8c73b5c4bb7dc8bb5d46651bd
Elian-beep <elianoliver101@gmail.com>
2022-11-10 08:55:29 -0300

    real baseurl

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export const http = axios.create({
-    baseURL: 'http://15.228.241.11:8081/amostras'
-    // 'https://back-api-xiloteca.vercel.app/'
+    baseURL: 'https://back-api-xiloteca.vercel.app/'
 });
