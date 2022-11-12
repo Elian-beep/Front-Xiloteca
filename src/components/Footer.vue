0c630619c26723c84216db9958284be12c760078
Elian-beep <elianoliver101@gmail.com>
2022-11-11 22:44:56 -0300

    footer tablet

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,13 @@
 <template>
   <footer :class="{ menuOpen: menuOpen }">
     <div class="area-logos">
-      <img src="@/assets/fapeam.png" alt="Logo Fapeam" />
-      <img src="@/assets/uea.png" alt="Logo UEA" />
-      <img
+      <img class="logo-fapeam" src="@/assets/fapeam.png" alt="Logo Fapeam" />
+      <img class="logo-uea" src="@/assets/uea.png" alt="Logo UEA" />
+      <img class="logo-cc"
         src="@/assets/cienciacomputacao.png"
         alt="Logo da turma de Ciência da Computação"
       />
-      <img src="@/assets/proex.png" alt="Logo Proex" />
+      <img class="logo-proex" src="@/assets/proex.png" alt="Logo Proex" />
     </div>
     <div class="area-text">
       <div class="credits">
@@ -54,7 +54,7 @@ footer {
 }
 
 .area-text {
-  border-top: 1px solid white;
+  border-top: .5px solid rgba(250, 250, 250, 0.5);
   display: flex;
   flex-direction: column;
   gap: 8px;
@@ -87,6 +87,53 @@ footer.menuOpen {
 
 @media screen and (min-width: 481px) {
   /* TABLET */
+
+  footer {
+    flex-direction: row;
+    padding: 34px 24px;
+  }
+
+  .area-logos{
+    max-width: 50%;
+    gap: 24px;
+    align-items: center;
+  }
+
+  .area-logos .logo-fapeam {
+    width: 38px;
+    height: 40px;
+  }
+
+  .area-logos .logo-uea {
+    width: 38px;
+    height: 37px;
+  }
+
+  .area-logos .logo-cc {
+    width: 47px;
+    height: 39px;
+  }
+
+  .area-logos .logo-proex {
+    width: 70px;
+    height: 19px;
+  }
+
+  .area-text {
+    padding-left: 24px;
+    width: 50%;
+    border-top: none;
+    border-left: 1px solid rgba(250, 250, 250, 0.5);
+  }
+
+  .credits {
+    flex-direction: column;
+    gap: 8px;
+  }
+
+  .area-text span {
+    text-align: left;
+  }
 }
 
 @media screen and (min-width: 769px) {
--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const http = axios.create({
-    baseURL: ''
+    baseURL: 'http://15.228.241.11:8081'
     // URL LOCAL:       http://localhost:8081
     // URL LOCAL-AWS: http://15.228.241.11:8081
     // URL OFICIAL:     https://back-api-xiloteca.vercel.app/
